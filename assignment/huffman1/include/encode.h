bb44da76a2daa991eeb4c0dd6a9c4de528653e7b
Yuto Nakamura <maron07222010@gmail.com>
2022-01-25 19:38:03 +0900

    assginment added

---
+++ assignment/huffman1/include/encode.h
@@ -0,0 +1,16 @@
+#pragma once
+
+typedef struct node Node;
+   
+struct node{
+    int symbol;
+    int count;
+    Node *left;
+    Node *right;
+};
+
+// ファイルをエンコードし木のrootへのポインタを返す
+Node *encode(const char *filename);
+// Treeを走査して表示する
+void traverse_tree(const int depth, const Node *root);
+
